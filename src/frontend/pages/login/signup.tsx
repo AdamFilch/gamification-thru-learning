6a577be9eff065864a355d414a4559235442a9a8
Adam Filchoir <filchoiradam@gmail.com>
2023-06-17 21:28:59 +0800

    Log In Style

--- src/frontend/pages/login/signup.tsx
+++ src/frontend/pages/login/signup.tsx
@@ -1,12 +1,9 @@
 import React, { useState } from 'react'
 import FormInputs from './forminputs'
-import { Formik } from "formik";
 import * as yup from "yup";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { setLogin } from "../../shared/index.ts";
-import Dropzone from "react-dropzone";
 import axios from "axios";
+import CloseIcon from '@mui/icons-material/Close';
 
 const registerSchema = yup.object().shape({
   username: yup.string().required("Required"),
@@ -21,6 +18,8 @@ interface formVals {
 
 const LoginForm = () => {
 
+  const navigate = useNavigate();
+
   const [values, setValues] = useState({
     firstname: "",
     username: "",
@@ -33,8 +32,8 @@ const LoginForm = () => {
       id: 1,
       name: "firstname",
       type: "text",
-      placeholder: "First Name",
-      errormessage: "Enter in the Name we should call you by.",
+      placeholder: "Enter your First Name",
+      errormessage: "*Enter in the Name we should call you by.",
       label: "First Name",
       required: true,
     },
@@ -42,8 +41,8 @@ const LoginForm = () => {
       id: 2,
       name: "username",
       type: "text",
-      placeholder: "Username",
-      errormessage: "Username should be 3-16 characters and shouldn't include any special character!",
+      placeholder: "Enter a Username",
+      errormessage: "*Username should be 3-16 characters and shouldn't include any special character!",
       label: "Username",
       pattern: "^[A-Za-z0-9]{3,16}$",
       required: true,
@@ -52,10 +51,10 @@ const LoginForm = () => {
       id: 3,
       name: "password",
       type: "password",
-      placeholder: "Password",
-      errormessage: "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      placeholder: "Enter a Password",
+      errormessage: "*Password should be 5-20 characters and include at least 1 letter, 1 number and 1 special character!",
       label: "Password",
-      //pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      //pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{5,20}$`,
       required: true,
     },
     {
@@ -63,7 +62,7 @@ const LoginForm = () => {
       name: "confirmPassword",
       type: "password",
       placeholder: "Confirm Password",
-      errormessage: "Passwords don't match!",
+      errormessage: "*Passwords don't match!",
       label: "Confirm Password",
       pattern: values.password,
       required: true,
@@ -103,16 +102,21 @@ const LoginForm = () => {
   }
   
 
+
   return (
     <div>
-      <div>
-        <form id="signup-form" onSubmit={handleSubmit}>
+      <button className='absolute p-7' onClick={() => navigate("/") }><CloseIcon sx={{fontSize: 50}}/></button>
+      <div className='flex align-middle justify-center text-[30px] pt-[50px] pb-9'>Register Now</div>
+      <div className='flex align-middle w-[100%] h-[100%] justify-center'>
+        
+        <form id="signup-form" onSubmit={handleSubmit} noValidate>
 
           {inputs.map((inputs) => (
             <FormInputs key={inputs.id} {...inputs} onChange={onChange}
             />
           ))}
-          <button type="submit" >Submit</button>
+          <button className='  p-3 text-blue-800 underline end-full' onClick={() => navigate("/SignIn")}>Already have an Account?</button>
+          <button className='w-[100%] h-[50px] p-[9px] border font-bold text-xl rounded-md bg-gray-200 group-invalid:pointer-events-none group-invalid:opacity-30' type="submit" >Submit</button>
         </form>
       </div>
     </div>
