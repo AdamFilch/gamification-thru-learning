545a9a35299d09ed82cd8421595a3736a5449046
Adam Filchoir <filchoiradam@gmail.com>
2023-06-25 00:59:50 +0800

    Master homepage

--- src/frontend/pages/master/buttons.tsx
+++ src/frontend/pages/master/buttons.tsx
@@ -4,20 +4,22 @@ import { ChartBarIcon, MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
 
 type Props = {
     content: number;
+    label: string;
+
 }
 
-const AddDel = ({content}: Props) => {
+const AddDel = ({content, label,}: Props) => {
     const contentDecider = (content: number) => {
         switch(content) {
             case 1:
                 return <div className=' flex flex-col'>
                             <PlusIcon className='h-[50px] w-[50px] m-auto'/>
-                    <span className=' text-[20px]'>Add a Learn</span>    
+                    <span className=' text-[20px]'>{label}</span>    
                 </div>
             case 2:
                 return <div className=' felx flex-col'>
                     <MinusIcon className='h-[50px] w-[50px] m-auto'/>
-                    <span className=' text-[20px] '> Delete a Learn</span>
+                    <span className=' text-[20px] '>{label}</span>
                 </div>
             case 3:
                 return <div>
--- src/frontend/pages/master/index.tsx
+++ src/frontend/pages/master/index.tsx
@@ -9,23 +9,25 @@ const Master = (props: Props) => {
         <div className=' m-auto flex flex-col pt-3'>
             <span className=' text-center text-[24px] pb-4'>Add a learn Video/Document</span>
             <div className=' flex flex-row m-auto space-x-16'>
-                <AddDel content={1} />
-                <AddDel content={2}/>
-                <AddDel content={3}/>
+                <AddDel content={1} label='Add a Learn post'/>
+                <AddDel content={2} label='Add a Learn post'/>
+                
             </div>
         </div>
         <div className=' m-auto flex flex-col pt-10'>
             <span className=' text-center text-[24px]  pb-4'>Add a word to Word Scramble</span>
             <div className=' flex flex-row m-auto space-x-16'>
-                <AddDel content={1}/>
-                <AddDel content={2}/>
+                <AddDel content={1} label='Add a Word'/>
+                <AddDel content={2} label='Delete a Word'/>
+                <AddDel content={3} label='Add a Learn post'/>
             </div>
         </div>
         <div className=' m-auto flex flex-col pt-10'>
             <span className=' text-center text-[24px]  pb-4'>Add another card to the Quiz</span>
             <div className=' flex flex-row m-auto space-x-16'>
-                <AddDel content={1}/>
-                <AddDel content={2}/>
+                <AddDel content={1} label='Add a Quiz Card'/>
+                <AddDel content={2} label='Delete a Quiz Card'/>
+                <AddDel content={3} label='Add a Learn post'/>
             </div>
         </div>
     </div>
