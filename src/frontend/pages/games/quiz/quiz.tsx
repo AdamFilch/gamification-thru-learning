5a7ef94433a831148037f505174f4c666a3037da
Adam Filchoir <filchoiradam@gmail.com>
2023-06-24 13:49:17 +0800

    quiz done

--- src/frontend/pages/games/quiz/quiz.tsx
+++ src/frontend/pages/games/quiz/quiz.tsx
@@ -1,5 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import CloseIcon from '@mui/icons-material/Close';
 
 type Props = {}
 
@@ -13,8 +15,8 @@ type questionKey = {
 }
 
 const Quiz = (props: Props) => {
-    
-    let questionOrder: questionKey[] = []
+    const navigate = useNavigate();
+    const[questionOrder, setQuestionOrder] = useState<questionKey[]>([])
 
     useEffect(() => {
         getQuestions();
@@ -30,9 +32,10 @@ const Quiz = (props: Props) => {
             let j = Math.floor(Math.random() * (i + 1));
             [dataArray[i], dataArray[j]] = [dataArray[j], dataArray[i]];
         }
-          questionOrder = dataArray as questionKey[];
-          setCurrentQuestionNo(0);
-          setCurrentQuestion(questionOrder[currentQuestionNo])
+          setQuestionOrder(dataArray);
+          setTotalQuestions(dataArray.length)
+          setCurrentQuestion(dataArray[0])
+          console.log()
 
         }).catch(() => {
           alert('Error Received')
@@ -40,46 +43,48 @@ const Quiz = (props: Props) => {
         return null;
       }
 
-
-    const [currentQuestionNo, setCurrentQuestionNo] = useState(0);
+    const [totalQuestions, setTotalQuestions] = useState(0)
+    const [currentQuestionNo, setCurrentQuestionNo] = useState(1);
     const [currentQuestion, setCurrentQuestion] = useState<questionKey>();
     // const [nextQuestion, setNextQuestion] = useState()
     const [showScore, setShowScore] = useState(false);
     const [score, setScore] = useState(0);
 
-    let i = 0;
     const answer = false;
+
     const AnswerOptionsClick = (answer: boolean) => {
         if(answer) {
             setScore(score + 1);
         }
-        // const nextQuestion = questionOrder[currentQuestion + 1];
-        if(i < questionOrder.length) {
-            
+        setCurrentQuestionNo(currentQuestionNo + 1)
+        
+        if(currentQuestionNo < totalQuestions) {
+            setCurrentQuestion(questionOrder[currentQuestionNo])
         } else {
             setShowScore(true);
         }
     }
 
   return (
-    <div>
-        <div>
+    <div className=' flex h-screen'>
+        <button className='absolute p-7' onClick={() => navigate("/Home") }><CloseIcon sx={{fontSize: 50}}/></button>
+        <div className=' m-auto flex w-[700px] h-[400px] border'>
             {showScore ? (
-                <div>You scored {score} out of {questionOrder.length} </div>
+                <div className='m-auto text-[40px] font-bold'>You scored {score} out of {totalQuestions} </div>
             ) : (
-                <div>
-                    <div>
-                        <div>
-                            <span>Question {currentQuestionNo + 1}/</span>10
+                <div className='flex m-auto w-[700px] h-[400px] border'>
+                    <div className='flex flex-col w-[300px] border '>
+                        <div className='flex pt-[40px] pl-[37px] '>
+                            <span className=' font-bold text-[33px]'>Question {currentQuestionNo}</span><span className='text-[22px]'>/{totalQuestions}</span>
                         </div>
-                        <div>{currentQuestion?.question}</div>
+                        <div className='p-4 pt-[20px] text-[22px]'>{currentQuestion?.question}</div>
                     </div>
-                    <div>
-                        <div>
+                    <div className='flex m-auto'>
+                        <div className='inline-flex flex-col space-y-5'>
                             {currentQuestion?.options.map((option) => (
-                                <button>{option.option}</button>
+                                <button className='border text-lg h-[70px] w-[300px]' onClick={() => AnswerOptionsClick(option.answer)}>{option.option}</button>
 
-                            ))};
+                            ))}
                         </div>
                     </div>
                 </div>
