1c34bce4807923055045587296faacc1e18cce4f
Adam Filchoir <filchoiradam@gmail.com>
2023-06-25 12:01:48 +0800

    edit wordscramble

--- src/frontend/pages/games/wordscramble/wordscramble.tsx
+++ src/frontend/pages/games/wordscramble/wordscramble.tsx
@@ -24,6 +24,8 @@ const WordScramble = (props: Props) => {
   });
     const ref =React.useRef<NodeJS.Timeout | null>(null);
     const [answer, setAnswer] = useState("");
+    const [endGame, setEndGame] = useState(false);
+    const [isCorrect, setIsCorrect] = useState(false);
 
     useEffect(() => {
         initGame();
@@ -71,6 +73,7 @@ const WordScramble = (props: Props) => {
       
 
     const initGame = () => {
+        setEndGame(false);
         setAnswer("");
         setToggle(true);
         setSeconds(60);
@@ -90,13 +93,17 @@ const WordScramble = (props: Props) => {
         
         const userWord = answer?.toString().toLowerCase().replace(/\s/g, "");
         if(!userWord) return alert("Please enter the word to check!");
-        if(userWord !== word?.word) return alert(`Oops! ${userWord} is not a correct word`);
-        alert(`Congrats! ${word.word.toUpperCase()} is the correct word`);
-        initGame();
-
-
+        if(userWord !== word?.word) {
+          setIsCorrect(false);
+        } else {
+          setIsCorrect(true);
+        }
+        setEndGame(true)
+        // alert(`Congrats! ${word.word.toUpperCase()} is the correct word`);
         
     }
+
+
 
   return (
     <div className='flex h-screen'>
@@ -104,18 +111,42 @@ const WordScramble = (props: Props) => {
         <div className='flex m-auto w-[550px] h-[500px] border'>
         <div className='flex flex-col w-full'>
             <h2 className=' text-[28px] font-bold p-[20px] pb-[10px] border-b'>Word Scramble</h2>
+            { endGame ? (
+              <div className=' w-[550px] h-[300px] flex flex-col justify-center m-auto'>
+                {isCorrect ? ( 
+                  
+                  <div className='flex flex-col '>
+                  <div className='m-auto text-[40px] font-bold'>CORRECT!</div>
+                  <div className='flex flex-row m-auto'>
+                  <span className=' text-[30px] font-bold pr-4'>{word?.word.toUpperCase()}</span><span className='text-[30px] font-bold'> IS THE WORD!</span>
+                  </div>
+                </div>
+
+                ) : (
+                  <div className='flex flex-col m-auto'>
+                    <div className=' flex m-auto text-[40px] font-bold'>Incorrect</div>
+                    <div className='flex flex-row m-auto'>
+                    <span className=' text-[30px] font-bold pr-4'>The Word is </span><span className=' text-[30px] font-bold'> {word?.word.toUpperCase()}</span>
+                    </div>
+                  </div>
+                )}
+                <button className=' flex p-[15px]  w-[300px] h-[100px] text-[30px] justify-center border m-auto' onClick={() => initGame()}>Play Again</button>
+              </div>
+            ) : (
             <div className=''>
-                <p className=' align-middle text-center p-9 tracking-[16px] text-[33px] font-semibold uppercase'>{shuffled?.word}</p>
-                <div className='pb-4'>
-                    <p className=' text-[20px] pl-7'>Hint: <span>{word?.hint}</span></p>
-                    <p className='text-[20px] pl-7 pt-2'>Time left: <span><b>{seconds}</b>s</span></p>
-                </div>
-                    <input className='flex m-auto w-[90%] border p-5 center' value={answer} type='text' onInput={handleChange} spellCheck='false' placeholder='Enter a Valid word' />
-                <div className='flex flex-row justify-center align-middle p-6 space-x-[80px]'>
-                    <button className='p-[15px] border' onClick={() => initGame()}>Refresh Word</button>
-                    <button className='p-[17px] border' onClick={() => checkWord()}>Check Word</button>
-                </div>
+            <p className=' align-middle text-center p-9 tracking-[16px] text-[33px] font-semibold uppercase'>{shuffled?.word}</p>
+            <div className='pb-4'>
+                <p className=' text-[20px] pl-7'>Hint: <span>{word?.hint}</span></p>
+                <p className='text-[20px] pl-7 pt-2'>Time left: <span><b>{seconds}</b>s</span></p>
             </div>
+                <input className='flex m-auto w-[90%] border p-5 center' value={answer} type='text' onInput={handleChange} spellCheck='false' placeholder='Enter a Valid word' />
+            <div className='flex flex-row justify-center align-middle p-6 space-x-[80px]'>
+                <button className='p-[15px] border' onClick={() => initGame()}>Refresh Word</button>
+                <button className='p-[17px] border' onClick={() => checkWord()}>Check Word</button>
+            </div>
+        </div>) }
+
+            
         </div>
         </div>
 
