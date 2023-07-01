ee5bd79177b77b8b050acc95a764145ed614baec
Adam Filchoir <filchoiradam@gmail.com>
2023-07-01 16:52:58 +0800

    comment section

--- src/frontend/pages/learn/commentsection.tsx/comment.tsx
+++ src/frontend/pages/learn/commentsection.tsx/comment.tsx
@@ -1,13 +1,38 @@
 import React from 'react'
 
-type Props = {}
+type Props = {
+  comment: commentsKey,
+  
+}
 
-const Comment = (props: Props) => {
+type commentsKey = {
+  author: string,
+  comment: string,
+  date: string,
+}
+
+const Commentbox = ({ comment}: Props) => {
+
+  const dateCal = () => {
+    
+  }
+
+
+
+
   return (
-    <div>
-        COMMENT
+    <div className='pt-3'>
+        <div className='w-full h-auto min-h-[50px]'>
+          <div className=' flex flex-row space-x-4'>
+            <span className=' font-bold text-[20px] align-middle'>{comment.author}</span>
+            <span className=' align-middle flex'>{comment.date}</span>
+          </div>
+          <div>
+            <span className=' pl-4 text-[17px]'>{comment.comment}</span>
+          </div>
+        </div>
     </div>
   )
 }
 
-export default Comment
+export default Commentbox
