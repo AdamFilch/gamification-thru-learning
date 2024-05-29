d8a15a74f84280f9633ebe5bf18e7a3c984ddadc
Adam Filchoir <filchoiradam@gmail.com>
2024-05-29 17:28:27 +0700

    Footer basic hover

--- src/frontend/pages/footer/Footer.tsx
+++ src/frontend/pages/footer/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer() {
   return (
     <div className="bg-highlight absolute bottom-auto left-0 w-full place-items-center">
-      <div className="  text-secondary lg:text-1xl lg:text-md mx-auto grid h-[300px] max-w-[1300px] grid-cols-2 grid-rows-1 gap-2 px-[50px] py-4 text-sm sm:grid-cols-4 sm:grid-rows-1 sm:px-[70px] sm:py-[70px]">
+      <div className="  text-secondary lg:text-1xl lg:text-md mx-auto grid h-[300px] max-w-[1300px] grid-cols-2 grid-rows-1 gap-2 px-[30px] py-5 text-sm sm:grid-cols-4 sm:grid-rows-1 sm:px-[70px] sm:py-[70px]">
         <div className=" col-span-2 flex flex-1 flex-col gap-1 text-left font-sans">
           <div className="text-text-secondary mb-2 text-[15px] font-semibold">
             GAMIFICATION OF LEARNING
@@ -14,25 +14,50 @@ export function Footer() {
             CONTACT INFO
           </div>
           <div>
-            <span>Portoflio</span>
+            <a
+              href="https://adamfilchoir.com/"
+              className=" hover:text-tertiary transition-all duration-300"
+            >
+              Portoflio
+            </a>
           </div>
           <div>
-            <span>Linkedin</span>
+            <a
+              href="https://www.linkedin.com/in/adamadamadam/"
+              className=" hover:text-tertiary transition-all duration-300"
+            >
+              Linkedin
+            </a>
           </div>
           <div>
-            <span>Github</span>
+            <a
+              href="https://github.com/AdamFilch"
+              className=" hover:text-tertiary transition-all duration-300"
+            >
+              Github
+            </a>
           </div>
         </div>
         <div className="flex flex-1 flex-col gap-1 md:gap-2">
           <div className=" text-text-secondary mb-2 text-[15px] font-semibold">
             TECH STACK
           </div>
-          <span>Typescript</span>
-          <span>React</span>
-          <span>Tailwind</span>
-          <span>MongoDB</span>
+          <span className=" hover:text-tertiary transition-all duration-300">
+            Typescript
+          </span>
+          <span className=" hover:text-tertiary transition-all duration-300">
+            React
+          </span>
+          <span className=" hover:text-tertiary transition-all duration-300">
+            Tailwind
+          </span>
+          <span className=" hover:text-tertiary transition-all duration-300">
+            MongoDB
+          </span>
           {/* <span>Express</span> */}
-          <span>YouTube API</span>
+          <span className=" hover:text-tertiary transition-all duration-300">
+            YouTube API
+          </span>
         </div>
       </div>
     </div>
