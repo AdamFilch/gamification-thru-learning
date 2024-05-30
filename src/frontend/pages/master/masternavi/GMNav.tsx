f6b61abc997fed321931d8b0664de998a4f9f1fc
Adam Filchoir <filchoiradam@gmail.com>
2024-05-30 19:27:43 +0700

    GMNav drop up menu

--- src/frontend/pages/master/masternavi/GMNav.tsx
+++ src/frontend/pages/master/masternavi/GMNav.tsx
@@ -7,7 +7,20 @@ export function GMNav() {
       <div className=" flex w-full rounded-2xl border-2 border-black md:hidden">
         <BuildIcon className="m-auto bg-white" fontSize="inherit" />
       </div>
-      {/* <div>Nav Bar</div> */}
+      <div className="absolute bottom-[120%] left-2 grid w-max grid-flow-row gap-2 rounded-2xl border-2 border-black p-3">
+        <div>Learn</div>
+        <div>
+          <div>Games</div>
+          <div className="grid grid-flow-row gap-2 pl-3 pt-2">
+            <span>Word Scramble</span>
+            <span>Quiz</span>
+            <span>Wordle</span>
+            <span>Drag-N-Drop</span>
+          </div>
+        </div>
+
+        <div>Users</div>
+      </div>
     </div>
   );
 }
